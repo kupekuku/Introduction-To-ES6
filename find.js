ba353f6feb2d0a85975aadc2381f0e79f927384d
moses.adebayo <moses.adebayo@venturegardengroup.com>
2018-07-13 10:20:49 +0100

    ....

--- find.js
+++ find.js
@@ -2,3 +2,5 @@ const ages = [18, 24, 26, 30, 31, 45, 60];
 const check1 = ages.find( a => a < 24); // 18
 const check2 = ages.findIndex( a => a === 30); //3
 const check3 = ages.findIndex( a => a === 6); //-1
+
+console.log(check1, check2, check3);
